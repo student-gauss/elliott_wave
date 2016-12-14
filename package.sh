001612a868019c43b0ed553182eac25231a13657
Student Gauss <student.gauss@gmail.com>
2016-12-14 07:37:23 -0800

    Add script to prepare the package.

---
+++ package.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+rm code.zip
+rm data.zip
+zip code.zip final/plotPredictorPerformance.py
+zip code.zip final/plot_trader_perf.py
+zip code.zip final/predictor.py
+zip code.zip final/trader.py
+zip code.zip final/testPredictor.py
+zip code.zip final/testTrader.py
+zip code.zip final/nytimes/download.py
+zip code.zip final/nytimes/nytimes.py
+zip code.zip final/nytimes/stanford_nlp.py
+
+zip data.zip data/*.csv
+zip data.zip final/predictor_perform_*.csv
+zip data.zip final/trader_perf_*.csv
+zip data.zip final/nytimes/nyt_comment_2014*.json
+zip data.zip final/nytimes/nyt_comment_2015*.json
+zip data.zip final/nytimes/nyt_comment_2016*.json
+zip data.zip final/nytimes/trends.json
+zip data.zip final/nytimes/trends_with_sentiment.json
